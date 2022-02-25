c0f49012f81c4ebab0f825d8c39cd33cb76dc9ea
Fahad niazi <fahadniazi142@gmail.com>
2022-02-25 17:37:20 +0400

    0.0.4

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 // Imports: Dependencies
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-community/async-storage";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import thunk from "redux-thunk";
